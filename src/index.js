b13d86ca4c78d988f0638fda203a06a88e3ce576
Ahmad Faqiri <ahmad.faqiri@ioof.com.au>
2020-01-10 16:57:18 +1100

    adding axios interceptors to intercept requests and responses

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios'
+
+// Global Axios interceptors to intercept requests/reponses sent/received to/from server
+
+axios.interceptors.request.use(request => {
+  // console.log(request)
+  return request;
+}, error => {
+  console.log(error);           // we may log error globally here to a file for example
+  return Promise.reject(error); // to pass the error back to caller
+})
+
+axios.interceptors.response.use(response => {
+  // console.log(response)
+  return response;
+}, error => {
+  console.log(error);           
+  return Promise.reject(error);
+})
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
